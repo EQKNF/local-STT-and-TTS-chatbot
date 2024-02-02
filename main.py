01111205a77dbc5fea0083cb90535694ab147db3
Emil QKNF <emil.fredriksen@hotmail.no>
2024-02-02 18:16:59 +0000

    Basic STT OpenAi whisper

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Hello world")
+import whisper
+
+model = whisper.load_model("base") 
+result = model.transcribe("health-and-wellness-advisor\heyhey.m4a", fp16=False, language="English")
+print(result["text"])
