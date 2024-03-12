fdbb35db81cc962eae55934f5d2eaea1097eca29
Emil QKNF <emil.fredriksen@hotmail.no>
2024-03-12 18:10:00 +0000

    Prep for sileroTTS

---
+++ test/silero_test.py
@@ -0,0 +1,22 @@
+# V4
+import os
+import torch
+
+device = torch.device('cpu')
+torch.set_num_threads(4)
+local_file = 'model.pt'
+
+if not os.path.isfile(local_file):
+    torch.hub.download_url_to_file('https://models.silero.ai/models/tts/en/v3_en.pt',
+                                   local_file)  
+
+model = torch.package.PackageImporter(local_file).load_pickle("tts_models", "model")
+model.to(device)
+
+example_text = 'Hello there! Currently testing text to speech on my pc. She sells sea shells by the sea shore'
+sample_rate = 48000
+speaker='en_1'
+
+audio_paths = model.save_wav(text=example_text,
+                             speaker=speaker,
+                             sample_rate=sample_rate)
